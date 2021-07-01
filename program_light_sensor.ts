c10957e832fd6782f67f19cd68af159042253abf
Calbabreaker <calbabreaker@gmail.com>
2021-07-01 10:17:45 +0930

    improved

--- program_light_sensor.ts
+++ program_light_sensor.ts
@@ -1,21 +1,27 @@
 const programLightSensor: IProgram = {
     name: "light sensor",
     run: () => {
-        ROBOT.csensor.setThreshold(Light.Dark, 10);
-        ROBOT.csensor.onLightDetected(LightIntensityMode.Reflected, Light.Dark, () => {
+        let mode = Light.Dark;
+        const detectFunc = () => {
             // don't do it if the robot is not moving
             if (ROBOT.motor.isReady()) return;
 
             ROBOT.motor.stop();
             moveForward(-10);
-            turn(90);
+            turn(45);
             if (
                 ROBOT.csensor.light(LightIntensityMode.Reflected) >
-                ROBOT.csensor.threshold(Light.Dark)
+                ROBOT.csensor.threshold(mode)
             ) {
                 moveForward(Infinity);
             }
-        });
+        }
+
+        const setMode = (newMode: Light) => {
+            mode = newMode;
+            ROBOT.csensor.onLightDetected(LightIntensityMode.Reflected, mode, detectFunc);
+            brick.showString(`Reflected Mode: ${mode == Light.Dark ? "Dark" : "Bright"}`, currentPrintLine + 2);
+        }
 
         brick.buttonEnter.onEvent(ButtonEvent.Pressed, () => {
             if (!ROBOT.motor.isReady()) return;
@@ -24,11 +30,25 @@ const programLightSensor: IProgram = {
         });
 
         brick.buttonDown.onEvent(ButtonEvent.Pressed, () => {
+            const threshold = mode == Light.Dark ? 5 : -5;
+            const calibratedLight = ROBOT.csensor.reflectedLight() + threshold;
             blinkLight(StatusLight.Orange, 100);
-            ROBOT.csensor.setThreshold(Light.Dark, ROBOT.csensor.reflectedLight() + 5);
+            ROBOT.csensor.setThreshold(mode, calibratedLight);
+            brick.showString(`Calibrated: ${calibratedLight}`, currentPrintLine + 1);
         });
 
-        println("Press up to calibrate light sensor.");
+        brick.buttonLeft.onEvent(ButtonEvent.Pressed, () => {
+            setMode(Light.Dark);
+        });
+
+        brick.buttonRight.onEvent(ButtonEvent.Pressed, () => {
+            setMode(Light.Bright);
+        });
+
+        clearScreen();
+        println("Press down to calibrate light sensor.");
         println("Press enter to start program.");
+        println("Press left use Dark mode and right to use Bright.");
+        setMode(Light.Dark);
     },
 };
